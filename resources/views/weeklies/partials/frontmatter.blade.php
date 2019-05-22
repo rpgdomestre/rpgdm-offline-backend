4064a6dddc26b4979a419accdc8aa3d29b7bf384
erick patrick <github@erickpatrick.net>
2019-05-22 22:14:54 +0200

    adds frontmatter template which will provide data to the next system

---
+++ resources/views/weeklies/partials/frontmatter.blade.php
@@ -0,0 +1,14 @@
+---
+number: {{ $edition }}
+mSubtitle: Edição {{ $edition }}
+date: '{{ $released_at  }}'
+extends: _layouts.markdown
+section: mContent
+mTitle: Weekly
+mActive: true
+mSubheader: true
+mNavigation: true
+mSectionPrimary: false
+mUseMTitleForPageTitle: true
+mContentClasses: weekly
+---
