1b3094c6c634ea369f056a082723ad77039e71b2
erick patrick <github@erickpatrick.net>
2019-05-22 21:37:28 +0200

    adds extra information into class docblocks
    this extra information allows PHPStorm to better know about properties
    and methods used in the classes which in turn does not complain about
    "missing" methods or "access to dynamic properties".

--- app/Link.php
+++ app/Link.php
@@ -6,6 +6,22 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * Class Link
+ *
+ * @package App
+ *
+ * @property int $id
+ * @property string $link
+ * @property string $name
+ * @property string $type
+ * @property int $section_id
+ * @property string $source
+ * @property ?int $via
+ * @property Carbon created_at
+ * @property Carbon $updated_at
+ *
+ */
 class Link extends Model
 {
     protected $fillable = [
@@ -21,11 +37,19 @@ class Link extends Model
         'created_at'
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function section(): BelongsTo
     {
         return $this->belongsTo(Section::class);
     }
 
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
     public function getCreatedAtAttribute(string $value): string
     {
         return Carbon::createFromDate($value)->diffForHumans();
--- app/Section.php
+++ app/Section.php
@@ -2,8 +2,20 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Section
+ *
+ * @package App
+ *
+ * @property int $id
+ * @property string $name
+ * @property Carbon created_at
+ * @property Carbon $updated_at
+ *
+ */
 class Section extends Model
 {
 }
