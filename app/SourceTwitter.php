1b24c3e7b29867b4900d00d6ef9c8be7bce77d0e
erick patrick <github@erickpatrick.net>
2019-05-26 13:43:25 +0200

    adds method to properly generate insert payload for multiple insert

--- app/SourceTwitter.php
+++ app/SourceTwitter.php
@@ -17,4 +17,34 @@ class SourceTwitter extends Model
         'source',
         'twitter'
     ];
+
+    /**
+     * @param array $sources
+     * @param array $twitters
+     * @param array $hides
+     *
+     * @return array
+     */
+    public function prepareMultipleInsertValues(
+        array $sources,
+        array $twitters,
+        array $hides
+    ): array {
+        $countSources = count($sources);
+
+        if ($countSources) {
+            $this->truncate();
+        }
+
+        return array_map(
+            static function (int $number) use ($sources, $twitters, $hides) {
+                return [
+                    'source' => $sources[$number],
+                    'twitter' => $twitters[$number],
+                    'hide' => array_key_exists($sources[$number], $hides),
+                ];
+            },
+            range(0, $countSources - 1)
+        );
+    }
 }
