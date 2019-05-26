761d60e61bbfe0f10f081b7776a8f2520fcd5692
erick patrick <github@erickpatrick.net>
2019-05-26 13:46:42 +0200

    adds Ide Helper package to make PHPStorm happy

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // DOMAIN
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
