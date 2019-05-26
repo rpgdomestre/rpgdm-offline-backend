7315109df1617f970688b7ab65e7563de1868196
erick patrick <github@erickpatrick.net>
2019-05-26 12:24:16 +0200

    adds template that will be used to show twitter handles for current weekly

---
+++ resources/views/weeklies/twitter.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Twitter users mentioned') }}</div>
+
+                    <div class="card-body">
+                        <div class="form-group">
+                            <textarea
+                                class="form-control"
+                                id="twitters"
+                                rows="10">@foreach ($all as $link){{ $link->twitter->twitter }} @endforeach</textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
