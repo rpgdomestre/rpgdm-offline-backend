b3c1144bdf19afeb7db05cb7486d8d434fb0a3cc
erick patrick <github@erickpatrick.net>
2019-09-02 20:41:31 +0200

    removes duplicated twitter handles from generate twitter mentions

--- app/Http/Controllers/WeekliesGenerateTwitter.php
+++ app/Http/Controllers/WeekliesGenerateTwitter.php
@@ -18,7 +18,11 @@ class WeekliesGenerateTwitter extends Controller
      */
     public function __invoke(Request $request, Weekly $weekly)
     {
-        $all = $weekly->fetchLinksForTwitter($weekly);
+        // gets all links than filter buy twitter handle without duplicates
+        $all = collect($weekly->fetchLinksForTwitter($weekly))
+            ->unique('twitter.twitter')
+            ->values()
+            ->all();
 
         return view('weeklies.twitter', compact('all'));
     }
