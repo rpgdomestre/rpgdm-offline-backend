81f44cde8de6e4fd3902e75d49db9bbb1d84c7d5
erick patrick <github@erickpatrick.net>
2019-09-17 00:27:06 +0200

    adds pagination to weeklies

--- app/Http/Controllers/WeekliesIndex.php
+++ app/Http/Controllers/WeekliesIndex.php
@@ -15,7 +15,8 @@ class WeekliesIndex extends Controller
      */
     public function __invoke(Request $request)
     {
-        $weeklies = Weekly::orderBy('edition', 'desc')->get();
+        $weeklies = Weekly::orderBy('edition', 'desc')
+            ->paginate(20);
 
         return view('weeklies.index', compact('weeklies'));
     }
