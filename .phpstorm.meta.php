744ddd55ffc4388dbb2f69857bba05cec7bfb211
erick patrick <github@erickpatrick.net>
2019-09-16 23:46:52 +0200

    updates lock file and IDE helpers

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-09-10 21:24:31.
+    * Generated on 2019-09-16 21:45:54.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
