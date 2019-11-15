f8c64ac7818f3bc31c1f62344403996d9184501f
Erick Patrick <erick.alvarenga@nintendo.de>
2019-11-15 15:47:45 +0100

    ensures creation of weekly files do not bring unnecessary frontmatter

--- resources/views/weeklies/partials/frontmatter.blade.php
+++ resources/views/weeklies/partials/frontmatter.blade.php
@@ -1,16 +1,8 @@
 ---
 number: {{ $edition }}
-mSubtitle: Edição {{ $edition }}
 date: '{{ $released_at  }}'
-extends: _layouts.markdown
+extends: _layouts.weekly
 section: mContent
-mTitle: Weekly
-mActive: true
-mSubheader: true
-mNavigation: true
-mSectionPrimary: false
-mUseMTitleForPageTitle: true
-mContentClasses: weekly
 description: >-
     {{ substr(str_replace(array("\r", "\n"), ' ', $description), 0, 299) }}
 ---
