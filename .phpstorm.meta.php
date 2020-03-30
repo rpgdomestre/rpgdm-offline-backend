c508bba129793f66abaaeff0e6a00b3596e943d9
erick patrick <github@erickpatrick.net>
2020-03-30 19:44:54 +0200

    updates PHPStorm helpers for Laravel

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-16 19:25:42.
+    * Generated on 2020-03-30 17:41:36.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
