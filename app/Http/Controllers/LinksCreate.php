6c7b21f59c1b4dd477c126cae34b344bec81ffb4
erick patrick <github@erickpatrick.net>
2020-04-09 02:55:08 +0200

    removes docblocks
    as we're using PHP 7.4 with type hints, return hints and soon strict
    types and property types, all these docblocks will no be needed.

--- app/Http/Controllers/LinksCreate.php
+++ app/Http/Controllers/LinksCreate.php
@@ -8,13 +8,6 @@ use Illuminate\Http\Request;
 
 class LinksCreate extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return void
-     */
     public function __invoke(Request $request, Weekly $weekly)
     {
         $sections = Section::all(['id', 'name']);
