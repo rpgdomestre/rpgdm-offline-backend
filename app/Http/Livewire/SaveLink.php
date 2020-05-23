e51b12cfc878b67ddb17af85de98633f5b6660e1
erick patrick <github@erickpatrick.net>
2020-05-23 15:46:57 +0200

    finishes off SaveLink component functionality
    - we now use one existing Request Validator instead of adding the rules
    directly inside the component;
    - we also add success/error messages where appropriated;
    - we also added previous values to form if customer types an invalid
    value;

--- app/Http/Livewire/SaveLink.php
+++ app/Http/Livewire/SaveLink.php
@@ -2,23 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use App\Http\Requests\StoreLinks;
 use App\Link;
-use Illuminate\Support\Collection;
-use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class SaveLink extends Component
 {
-    // rendering data
-    public $sections;
-
     // Link data
     public $edition;
     public $link;
     public $name;
     public $type;
     public $section_id;
-    public $source;
+    public $sourceName;
     public $via;
 
     public function updated($field)
@@ -26,33 +22,48 @@ class SaveLink extends Component
         $this->validateOnly($field, [
             'edition' => 'required|exists:weeklies,id',
             'link' => 'required|url|unique:links',
-            'via' => 'required_if:section_id,5',
         ]);
     }
 
     public function submit()
     {
-        $this->validate([
-            'link' => 'required|url|unique:links',
-            'name' => 'required',
-            'type' => [
-                'required',
-                Rule::in(['nacional', 'internacional', 'geral']),
-            ],
-            'section_id' => 'required|exists:sections,id',
-            'source' => 'required',
-            'via' => 'required_if:section_id,5',
-            'edition' => 'required|exists:weeklies,id',
+        $rules = (new StoreLinks)->rules();
+        $this->validate($rules);
+
+        $link = Link::firstOrCreate([
+            'edition' => $this->edition,
+            'link' => $this->link,
+            'name' => $this->name,
+            'type' => $this->type,
+            'section_id' => $this->section_id,
+            'source' => $this->sourceName,
+            'via' => $this->via,
         ]);
 
-        Link::firstOrCreate([
-            'edition' => $this->edition
-        ]);
+        $editLinkRoute = route('links.edit', ['link' => $link]);
+
+        session()->flash(
+            'status',
+            "Link <a href='{$editLinkRoute}'>{$link->name}</a> created!"
+        );
+
+        $edition = $this->edition;
+
+        // cleans up form
+        $this->reset();
+
+        $this->edition = $edition;
     }
 
-    public function mount($sections)
+    public function mount(int $edition, array $linkData)
     {
-        $this->sections = $sections;
+        $this->edition = old('edition', $edition);
+        $this->link = old('link', $linkData['link'] ?? '');
+        $this->name = old('name', $linkData['name'] ?? '');
+        $this->type = old('type', $linkData['type'] ?? '');
+        $this->section_id = old('section_id', $linkData['sectionId'] ?? 0);
+        $this->sourceName = old('sourceName', $linkData['sourceName'] ?? '');
+        $this->via = old('via', $linkData['via'] ?? '');
     }
 
     public function render()
