e82ee8879b512588dbc7d06c6ca04bc307670f9d
Erick Rocha <contato@erickpatrick.net>
2020-12-22 00:22:55 +0100

    makes use of recently created `GenerateMarkdown` Action
    This cleans the controller class making it simpler and allows
    a single place to have the MArkdown Generation logic.

--- app/Http/Controllers/Dashboard/WeekliesGenerateMarkdown.php
+++ app/Http/Controllers/Dashboard/WeekliesGenerateMarkdown.php
@@ -2,23 +2,23 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Actions\Rpgdm\GenerateMarkdown;
 use App\Models\Weekly;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class WeekliesGenerateMarkdown extends Controller
 {
+    private GenerateMarkdown $generator;
+
+    public function __construct(GenerateMarkdown $generator)
+    {
+        $this->generator = $generator;
+    }
     public function __invoke(Request $request, Weekly $weekly)
     {
-        $fetched = $weekly->fetchLinks($weekly);
-        $allLinks = $fetched['all'];
-        $groups = $fetched['groups'];
-
         $fileName = "{$weekly->edition}.blade.md";
-        $view = view(
-            'weeklies.markdown',
-            compact('weekly', 'allLinks', 'groups')
-        );
+        $view = $this->generator->execute($weekly);
 
         return response()->streamDownload(
             static function () use ($view) {
