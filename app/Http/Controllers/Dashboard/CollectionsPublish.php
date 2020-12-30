5522e6ff326ef54b2175c7e21069e4f5868a136f
erick patrick <github@erickpatrick.net>
2020-12-30 12:29:53 +0100

    adjusts collection class to also generate pagination results

--- app/Http/Controllers/Dashboard/CollectionsPublish.php
+++ app/Http/Controllers/Dashboard/CollectionsPublish.php
@@ -29,9 +29,10 @@ class CollectionsPublish extends Controller
             try {
                 $from = $metadata['from'] ?? $collection;
                 $to = $metadata['to'] ?? $collection;
+                $color = $metadata['color'] ?? 'black';
                 $entries = File::allFiles(base_path('content' . DIRECTORY_SEPARATOR . $from));
 
-                collect($entries)->each(function ($entry, $key) use ($to) {
+                $chunks = collect($entries)->map(function ($entry) use ($to) {
                     $path = $entry->getPathname();
                     $yaml = $this->markdownReader->getYaml($path);
                     $body = $this->markdownReader->getBody($path);
@@ -40,12 +41,55 @@ class CollectionsPublish extends Controller
                         'content' => $body,
                         'title' => $yaml['title'],
                         'color' => $yaml['color'] ?? '',
-                        'parent' => Str::title($yaml['parent'] ?? '')
+                        'parent' => Str::title($yaml['parent'] ?? ''),
+                        'description' => $yaml['description'] ?? ''
                     ]);
 
                     $metadata = $this->getFolderMeta($yaml, $to);
                     $this->publisher->save(metadata: $metadata, content: $content);
-                });
+
+                    array_shift($metadata);
+                    $url = implode(DIRECTORY_SEPARATOR, $metadata);
+                    return ['yaml' => $yaml, 'body' => $body, 'url' => $url];
+                })->chunk($collections[$collection]['chunk'] ?? 10);
+
+                // generate paginated pages as necessary
+                foreach ($chunks as $key => $chunk) {
+                    $previous = $key === 0 ? '' : $key - 1;
+                    $next = $key === count($chunks) - 1 ? '' : $key + 2;
+                    $page = $key + 1;
+                    $content = view(
+                        "pages.listing",
+                        [
+                            'chunk' => $chunk,
+                            'color' => $color,
+                            'next' => $next,
+                            'previous' => $previous,
+                            'collection' => $collection,
+                            'description' => "Página #{$page} de {$collection}",
+                        ]
+                    );
+                    if ($key === 0) {
+                        $this->publisher->save(
+                            metadata: [
+                                public_path(self::SAVE_TO),
+                                $collection
+                            ],
+                            content: $content
+                        );
+                    }
+                    $this->publisher->save(
+                        metadata: [
+                            public_path(self::SAVE_TO),
+                            $collection,
+                            $key + 1
+                        ],
+                        content: $content
+                    );
+                }
+                // first entry is the index.html from newer to older
+                // second goes folder /2/index.html then /3/index.html
+                // and so on and so forth
             } catch (DirectoryNotFoundException) {
                 $contents[$collection] = [];
             }
