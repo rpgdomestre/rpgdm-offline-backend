aa44dbb2ba3acb02d1168b5c1a7fb0dc41ea524b
erick patrick <github@erickpatrick.net>
2021-01-01 15:18:43 +0100

    adds lang attribute and meta tag to identify content language

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
     <head>
         <base href="{{ config('rpgdm.url') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="{{ $metaDescription }}">
         <link rel="stylesheet" href="{{ config('rpgdm.url')}}css/rpgdm.css">
         <link rel="icon" type="image/png" href="{{ config('rpgdm.url')}}images/mestre.png" />
+        <meta http-equiv="Content-Language" content="pt-br">
         <title>{{ $metaTitle }}</title>
     </head>
     <body>
