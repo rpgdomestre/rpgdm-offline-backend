dae0506470414bb877bf57ca1fec842e009196d9
erick patrick <github@erickpatrick.net>
2021-01-07 00:48:03 +0100

    updates collection index to use Models\Content
    - we also add a new collection attribute `isHidden` to tell user that that given collection will not generate a final customer page

--- app/Http/Controllers/Dashboard/CollectionsIndex.php
+++ app/Http/Controllers/Dashboard/CollectionsIndex.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Content;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Symfony\Component\Finder\Exception\DirectoryNotFoundException;
 
 class CollectionsIndex extends Controller
 {
+    private const HIDDEN_COLLECTION_PREFIX = '_';
+
+    public function __construct(
+        private Content $content
+    ) {}
+
     public function __invoke(Request $request)
     {
         $collections = config('rpgdm.collections');
@@ -16,12 +24,16 @@ class CollectionsIndex extends Controller
         foreach ($collections as $collection => $metadata) {
             try {
                 $from = $metadata['from'] ?? $collection;
-                $entries = count(File::allFiles(base_path('content' . DIRECTORY_SEPARATOR . $from)));
+                $entries = $this->content->getEntriesFor($from)->count();
+                $isHidden = Str::of($collection)->startsWith(
+                    self::HIDDEN_COLLECTION_PREFIX
+                );
             } catch (DirectoryNotFoundException) {
                 $entries = 0;
             }
 
             $collections[$collection]['entries'] = $entries;
+            $collections[$collection]['isHidden'] = $isHidden;
         }
 
         return view('collections.index', compact('collections'));
