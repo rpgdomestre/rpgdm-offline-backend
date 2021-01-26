c1df0bdff3be885cec220b597408dc2d795054b0
erick patrick <github@erickpatrick.net>
2021-01-26 20:38:25 +0100

    updates tailwind config to use 2 spaces

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './content/**/*.blade.md',
-        './public/**/*.html',
-    ],
+  purge: [
+    './vendor/laravel/jetstream/**/*.blade.php',
+    './storage/framework/views/*.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './content/**/*.blade.md',
+    './public/**/*.html',
+  ],
 
-    theme: {
-        extend: {},
+  theme: {
+    extend: {},
+  },
+
+  variants: {
+    extend: {
+      animation: ['hover'],
+      opacity: ['disabled'],
     },
+  },
 
-    variants: {
-        extend: {
-            animation: ['hover'],
-            opacity: ['disabled'],
-        },
-    },
-
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
