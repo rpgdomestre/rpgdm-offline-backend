80585c19378f969a68883dafb0c08d2c8f1fc1bf
erick patrick <github@erickpatrick.net>
2021-01-27 21:42:38 +0100

    adjusts MarkdownFile to access the right properties

--- app/Models/MarkdownFile.php
+++ app/Models/MarkdownFile.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
+use stdClass;
+
 class MarkdownFile
 {
-    public function __construct(private $entry) {}
+    public function __construct(private $entry)
+    {
+    }
 
     public function getPathname(): string
     {
-        return $this->entry->getPathname();
+        return $this->entry->file->getPathname();
+    }
+
+    public function getCollection(): stdClass
+    {
+        return $this->entry->collection;
     }
 }
